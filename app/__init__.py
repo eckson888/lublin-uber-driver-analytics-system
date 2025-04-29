f02ef23e6376b1aea735f5ace0f90cdc91170c6e
eckson888 <madudziak121@gmail.com>
2025-04-29 13:13:06 +0200

    flask setup

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+def create_app():
+    
+    app = Flask(__name__)
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    from .routes import main
+    app.register_blueprint(main)
+
+    return app
---
+++ app/routes.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def home():
+    return render_template('index.html')
+
+@main.route('/plot')
+def plot():    
+    return render_template('plot.html')
