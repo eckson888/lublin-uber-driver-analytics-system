ac1098539222dd3e3a1d3a42666e24b38a92d73f
eckson888 <madudziak121@gmail.com>
2025-05-04 13:25:44 +0200

    ladningPage

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,16 @@ module.exports = {
   content: ["./app/templates/**/*.html"],
   theme: {
     extend: { 
-      backgroundImage: {
-      'bg1': "url('/static/resources/bg.png')"  
-    }},
+      backgroundImage: 
+      {
+      'bg1': "url('/static/resources/bg_blur.png')"  
+      },
+      boxShadow:
+      {
+      'shad_big' : '0px 0px 60px 30px rgba(0, 0, 0, 0.95)',
+      'shad_button' : '0px 0px 49px -14px rgba(110, 0, 253, 1)'
+      }
+      },
   },
   plugins: [],
 }
